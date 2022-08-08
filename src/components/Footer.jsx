ae9db069d89a60cb45d7368b5384056943d3f8e5
Carla Pico <carlaspico@gmail.com>
2022-08-08 13:54:11 -0400

    added menubar main and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+    <footer>
+        <p>&copy; 2022 Carla Pico </p>
+    </footer>
+}
+
+export default Footer;
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Menubar () {
+    return  (
+        <nav>
+            Main
+        </nav>
+    )
+}
+
+export default Menubar
