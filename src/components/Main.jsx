b2331b9e471f36fbf69b37f39b8d473331ba4ea3
Carla Pico <carlaspico@gmail.com>
2022-08-08 14:15:58 -0400

    destructing the prop name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
-function Main(props){
-    const {name, total, isAmazing, fruit}= props
+// function Main(props){
+//     const {name, total, isAmazing, fruit}= props
+//     return (
+//         <main>
+//             {/* <h1> Hello {props.name} </h1> */}
+//             <h1> Hello {name}</h1>
+//         </main>
+//     )
+// }
+
+//same as 
+
+function Main({name, total, isAmazing, fruit}){
     return (
         <main>
-            {/* <h1> Hello {props.name} </h1> */}
             <h1> Hello {name}</h1>
         </main>
     )
 }
-
 export default Main;
