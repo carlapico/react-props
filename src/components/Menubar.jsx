ef7f9ce9c9f35f4911dfa20a3fe8e16d85e96a87
Carla Pico <carlaspico@gmail.com>
2022-08-08 15:03:26 -0400

    adding comment of what I am doing with passing down the propa and lifting up the state

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+//passing prop (name and SetName) from parent to child
+//lifting up state (setName) from child to parent
 function Menubar({name,setName}) {
     return  (
         <nav style={{display:'flex',justifyContent:'space-between'}}>
